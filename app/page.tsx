4f1f43caaa3b1666bfd656b113ed6a8f4f8369a0
SerjeiMikailov <felipemaders8@gmail.com>
2023-05-11 18:34:27 -0300

    g

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,29 @@
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className='ml-20 mt-7 '>
-    <h1 className="text-9x1">Hellow</h1>
-    </div>
-  )
+    return (
+        <div className="bg-gradient-to-b from-navy-900 to-navy-500">
+            <div className="ml-20 mt-7 ">
+                <h1 className="text-7xl flex justify-center sm:mt-20 lg:mt-27">
+                    Serjei's room
+                </h1>
+
+                <div className="flex justify-center sm:mt-20 lg:mt-27">
+                    <Link href="/about">
+                        <button className="text-2xl border-2 border-gray-500 hover:border-gray-200">
+                            Read about me
+                        </button>
+                    </Link>
+                </div>
+
+                <div className="flex justify-center sm:mt-5 lg:mt-7">
+                    <Link href="/works">
+                        <button className="text-2xl border-2 border-gray-500 hover:border-gray-200">
+                            My Workshop
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 }
