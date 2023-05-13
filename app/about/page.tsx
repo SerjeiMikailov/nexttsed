4a55ac2d7e13b9f08d7635de7a576d92cb90317f
SerjeiMikailov <felipemaders8@gmail.com>
2023-05-13 14:46:16 -0300

    g

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,68 @@
+import Link from "next/link";
+
 export default function About() {
-    return <h1 className="text-red-500">About</h1>;
+    return (
+        <>
+             <div className="flex justify-center mt-8 mb-4">
+                <h1 className="text-7xl">About me</h1>
+            </div>
+
+            <div className="flex justify-center">
+                <h3>(Boring text below)</h3>
+            </div>
+
+            <div className="ml-100 mt-24">
+                <h4 className="font-bold mb-4 mr-24">Let's talk about with some asks</h4>
+
+                <li>
+                    I'm human?
+                </li>
+                <li>
+                    Why my nickname is Serjei Mikailov?
+                </li>
+                <li>
+                    My objectives?
+                </li>
+
+                <p className="mt-4 w-10/12">
+                    Off course I'm human (i think...) My nickname is Serjei Mikailov inspired by a russian mobster, for no particular reason.
+                     I'm very attracted to study Computer Enginnering, hardware and low level programming.
+                    I'm a descentralized systems enjoyer and intending to live in USA or some european east country,
+                    maybe Poland.
+                     About hobbies i like to play competitive games as LOL, CSGO, and survival games as ARK and RUST, i also like philosophy and economy. Currently i'm studying Rust, C++
+                    and a little bit about OpenGL.
+
+                    And yes, i'm studying English, so i don't speak fluently as you see...
+                </p>
+            </div>
+
+            <div className="ml-100 mt-10">
+
+                <h4 className="mb-3 font-bold">My steam if you want to add me</h4>
+
+                <img
+                        src="https://cdn.discordapp.com/attachments/697977914402144256/1106999183027753011/image.png"
+                        alt="steam image"
+                        className="mb-5"
+                    />
+                <a
+                    href="https://steamcommunity.com/id/serjeimikailov/"
+                    className="mt-8 underline"
+                    target="blank"
+                >
+                    <p className="text-white transition-colors hover:text-blue-200">
+                        My steam profile
+                    </p>
+                </a>
+            </div>
+
+            <div className="ml-100 mt-5 mb-5">
+                <Link href="/">
+                    <button className="text-2xl rounded-full bg-blue-500 text-white px-4 py-2 hover:border-gray-200">
+                        <p>Back to main page</p>
+                    </button>
+                </Link>
+            </div>
+        </>
+    )
   }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,66 +1,91 @@
-import React from 'react';
-import Link from "next/link"
+import React from "react";
+import Link from "next/link";
 
 export default function Works() {
-  return (
-    <>
-    <div className="flex justify-center mt-8 mb-4">
-          <h1 className="text-7xl">Projects</h1>
-      </div>
+    return (
+        <>
+            <div className="flex justify-center mt-8 mb-4">
+                <h1 className="text-7xl">Projects</h1>
+            </div>
 
-      <div className='flex justify-center'>
-        <h3>Only personal projects</h3>
-        </div>
+            <div className="flex justify-center">
+                <h3>Only personal projects</h3>
+            </div>
 
-      <div className="ml-100 mr-soph mt-14 rounded border border-white border-r-2 border-r-1/4 p-4">
-        <h4 className='font-bold text-2xl'>Soph bot</h4>
-        <p className='mb-2 mt-1'>My personal discord bot, configured to meet the needs of my server</p>
-        <img
-            src="https://cdn.discordapp.com/attachments/697977914402144256/1106618192119472189/soph.PNG"
-            alt="soph image"
-            className="mb-1"
-         />
-         <a href='https://github.com/SerjeiMikailov/soph_bot' className='mt-8'>
-            <p className='text-white transition-colors hover:text-blue-200'>Github Repository</p>
-         </a>
-      </div>
+            <div className="ml-100 mr-soph mt-14 rounded border border-white border-r-2 border-r-1/4 p-4">
+                <h4 className="font-bold text-2xl">Soph bot</h4>
+                <p className="mb-2 mt-1">
+                    My personal discord bot, configured to meet the needs of my
+                    server
+                </p>
+                <img
+                    src="https://cdn.discordapp.com/attachments/697977914402144256/1106618192119472189/soph.PNG"
+                    alt="soph image"
+                    className="mb-1"
+                />
+                <a
+                    href="https://github.com/SerjeiMikailov/soph_bot"
+                    className="mt-8"
+                    target="blank"
+                >
+                    <p className="text-white transition-colors hover:text-blue-200">
+                        Github Repository
+                    </p>
+                </a>
+            </div>
 
-      <div className='ml-100 mt-14 mb-14 mr-3 rounded border border-white border-r-2 border-r-1/4 p-4'>
-        <h4 className='font-bold text-2xl'>GPT Fanfic machine</h4>
-        <p className='mb-2 mt-1'>Chat GPT Api test but with voice commands</p>
-        <img
-            src="https://cdn.discordapp.com/attachments/1099395583585816746/1099395627055595574/image.png"
-            alt="soph image"
-            className="mb-1"
-         />
-         <a href='https://github.com/SerjeiMikailov/gPT' className='mt-8'>
-            <p className='text-white transition-colors hover:text-blue-200'>Github Repository</p>
-         </a>
-      </div>
+            <div className="ml-100 mt-14 mb-14 mr-3 rounded border border-white border-r-2 border-r-1/4 p-4">
+                <h4 className="font-bold text-2xl">GPT Fanfic machine</h4>
+                <p className="mb-2 mt-1">
+                    Chat GPT Api test but with voice commands
+                </p>
+                <img
+                    src="https://cdn.discordapp.com/attachments/1099395583585816746/1099395627055595574/image.png"
+                    alt="soph image"
+                    className="mb-1"
+                />
+                <a
+                    href="https://github.com/SerjeiMikailov/gPT"
+                    className="mt-8"
+                    target="blank"
+                >
+                    <p className="text-white transition-colors hover:text-blue-200">
+                        Github Repository
+                    </p>
+                </a>
+            </div>
 
-      <div className='ml-100 mt-14 mb-14 mr-imc rounded border border-white border-r-2 border-r-1/4 p-4'>
-        <h4 className='font-bold text-2xl'>IMC Calculator</h4>
-        <p className='mb-2 mt-1'>IMC Calculator website (Currently outdated)</p>
-        <img
-            src="https://cdn.discordapp.com/attachments/697977914402144256/1106628833739804712/image.png"
-            alt="soph image"
-            className="mb-1"
-         />
-         <a href='https://github.com/SerjeiMikailov/calc-imc' className='mt-8'>
-            <p className='text-white transition-colors hover:text-blue-200'>Github Repository</p>
-         </a>
-      </div>
-      <div className='ml-100'>
-            <h2 className='text-4xl'>More coming soon...</h2>
-            <p>(to view my other projects go to my github)</p>
-      </div>
-      <div className='ml-100 mt-5 mb-5'>
-            <Link href="/">
-                <button className="text-2xl rounded-full bg-blue-500 text-white px-4 py-2 hover:border-gray-200">
-                    <p>Back to main page</p>
-                </button>
-            </Link>
-        </div>
-    </>
-  )
+            <div className="ml-100 mt-14 mb-14 mr-imc rounded border border-white border-r-2 border-r-1/4 p-4">
+                <h4 className="font-bold text-2xl">IMC Calculator</h4>
+                <p className="mb-2 mt-1">
+                    IMC Calculator website (Currently outdated)
+                </p>
+                <img
+                    src="https://cdn.discordapp.com/attachments/697977914402144256/1106628833739804712/image.png"
+                    alt="soph image"
+                    className="mb-1"
+                />
+                <a
+                    href="https://github.com/SerjeiMikailov/calc-imc"
+                    className="mt-8"
+                    target="blank"
+                >
+                    <p className="text-white transition-colors hover:text-blue-200">
+                        Github Repository
+                    </p>
+                </a>
+            </div>
+            <div className="ml-100">
+                <h2 className="text-4xl">More coming soon...</h2>
+                <p>(to view my other projects go to my github)</p>
+            </div>
+            <div className="ml-100 mt-5 mb-5">
+                <Link href="/">
+                    <button className="text-2xl rounded-full bg-blue-500 text-white px-4 py-2 hover:border-gray-200">
+                        <p>Back to main page</p>
+                    </button>
+                </Link>
+            </div>
+        </>
+    );
 }
